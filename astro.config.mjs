b9085338e1b622d74f9b137cb97b81447f4a622a
Pablito0403 <pa0403.pablo@gmail.com>
2025-11-17 01:49:04 -0300

    Ver si no revienta en produccion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import db from '@astrojs/db';
-
 import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
